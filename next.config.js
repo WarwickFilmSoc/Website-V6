6b97b6e9db49217f7cb804f4de12a50ad566c9aa
Josh Heng <josh@joshheng.co.uk>
2023-08-07 18:33:20 +0100

    :wrench: Add redirects for old website

--- next.config.js
+++ next.config.js
@@ -9,6 +9,48 @@ const nextConfig = {
       },
     ],
   },
+  redirects: async () => [
+    // General
+    {
+      source: '/terms',
+      destination: '/terms-and-conditions',
+      permanent: true,
+    },
+
+    // Website V2 Constitution
+    {
+      source: '/info/constitution',
+      destination: '/constitution',
+      permanent: true,
+    },
+    {
+      source: '/info/execroles',
+      destination: '/constitution/exec-roles',
+      permanent: true,
+    },
+    {
+      source: '/info/committees',
+      destination: '/constitution/committees',
+      permanent: true,
+    },
+    {
+      source: '/info/keyagreement',
+      destination: '/constitution/key-agreement',
+      permanent: true,
+    },
+
+    // Website V2 Terms
+    {
+      source: '/info/tickettc',
+      destination: '/terms-and-conditions',
+      permanent: true,
+    },
+    {
+      source: '/info/loyaltytc',
+      destination: '/terms-and-conditions#loyalty',
+      permanent: true,
+    },
+  ],
 };
 
 module.exports = nextConfig;
