26dc705c226db1704d568af76444235d34cd6986
Josh Heng <josh@joshheng.co.uk>
2023-08-11 14:52:07 +0100

    :speech_balloon: Add technical page content from Website V2

--- src/app/(about)/technical-specifications/page.tsx
+++ src/app/(about)/technical-specifications/page.tsx
@@ -7,19 +7,258 @@ export default function About() {
         <Link href="/about">About Us</Link>
       </p>
       <h1 className="mb-1">Technical Specifications</h1>
-      <p className="mb-2">
-        Warwick Student Cinema is a student run, professional standard cinema
-        located in the University of Warwick L3 Lecture Theatre.
+      <h2>Sight&hellip;</h2>
+
+      <p>
+        The first thing you should know about Warwick Student Cinema is that we
+        do not show DVDs, Blu-rays or any other home cinema format media to our
+        audience in L3. Only industry standard equipment is used; this means
+        either showing either real film &ndash; both 35mm and 70mm &ndash; or
+        DCI compliant digitally-projected movies.
       </p>
-      <p className="mb-2">
-        For the past 50 years, we have provided a top-quality cinema experience.
+
+      <h3>35mm and 70mm Film</h3>
+
+      <p>
+        Unlike many other cinemas &ndash; even those still able to show 35mm or
+        70mm film &ndash; Warwick Student Cinema usually uses a changeover
+        system when running shows from these formats, in which two projectors
+        are used alternately. Films are transported on spools of about twenty
+        minutes each. A typical feature will have six or seven reels. One reel
+        is played on the first projector and at the right moment at the end of
+        this reel, we switch over to the second projector and so on. This moment
+        is indicated by marks on the film (as made famous in <i>Fight Club</i>!)
+        known as &lsquo;cue dots&rsquo;, which appear in the top right hand
+        corner of the image. There are two sets of dots; the first is
+        approximately eight seconds before the end of the reel and indicates to
+        the projectionist when to start the film moving through the second
+        projector. The second set, about seven seconds later, signals the time
+        to shut off the first projector and begin showing the next reel of film.
+        This results in an almost seamless changover that you, the audience,
+        should not notice.
       </p>
-      <h2 className="mt-4">Tickets</h2>
-      <p className="mb-2">Buy tickets pls.</p>
-      <h2 className="mt-4">Warwick Film Society</h2>
-      <p className="mb-2">Members are cool.</p>
-      <h2 className="mt-4">Getting Involved</h2>
-      <p className="mb-2">Once you&apos;re a member, you can join crew.</p>
+
+      <p>
+        Not only are we able to project standard 35mm film, but also our
+        projectors are capable of handling 70mm film. We are the only student
+        cinema in the country capable of showing 70mm and one of only a handful
+        of cinemas in the UK who show 70mm film on a regular basis. This
+        provides an improved picture due to two reasons: firstly, the film is
+        often in better condition than a 35mm print &ndash; as it will have been
+        played far less &ndash; and secondly, because the film is larger,
+        meaning the image is magnified less than 35mm.
+      </p>
+
+      <h3>Our Platter System</h3>
+
+      <p>
+        Warwick Student Cinema is also able to operate with a long play system.
+        This involves, in our case, a platter which was kindly given to us on
+        permanent loan by the
+        <a href="http://www.ppttrust.org/">Projected Picture Trust</a> in 2002.
+        The platter consists of three giant plates, each approximately 54 inches
+        in diameter. The entire film is spliced together and placed on one of
+        the plates. The film then makes its way to one of our Victoria 8
+        projectors (from which it is projected on screen) via a circuitous route
+        using a series of rollers, before going back to one of the other plates.
+        This is particularly useful in the event of one of the projectors
+        failing, an event which previously would have resulted in the
+        cancellation of the film. Also, unlike any other playing system, films
+        can immediately be shown again due to the fact that the film uptake is
+        from the inside of the reel.
+      </p>
+
+      <h3>Digital Cinema</h3>
+
+      <p>
+        Our society is also lucky enough to have installed the first 4K RGB
+        laser projector in Coventry and Warwickshire, in February 2020. Unlike
+        traditional cinema projectors (both digital and film), its light source
+        comprises red, green and blue laser modules instead of a xenon arc lamp.
+        This cutting-edge illumination technology facilitates superior black
+        levels as well as a wider colour gamut to provide the best picture
+        possible. The 4K resolution provides a four-fold increase in picture
+        detail compared to standard 2K systems.
+      </p>
+
+      <p>
+        The digital projector &ndash; a Barco SP4k-15 &ndash; is adapted for use
+        with its B-mount high contrast lens and has an ICMP-X installed, which
+        combines the roles of a digital cinema server and alternative content
+        processing (such as the pre-film slideshow). Naturally, our digital
+        projector does not operate using a changeover system as there are no
+        reels of film to change over. Instead, the film arrives at the cinema on
+        a hard drive, or is transfered electronically onto one of our film
+        transit servers. The film, stored in a format known as DCP, is then
+        &lsquo;ingested&rsquo; (uploaded) to the digital projector server; this
+        takes about three-quarters of the time taken to play the whole of the
+        film. A playlist on the server is then created, which includes
+        auditorium automation (lights, moveable screen masking etc.), pre-film
+        trailers and &ndash; of course &ndash; the feature film. This automation
+        takes a lot of pressure off of the projectionist on the night!
+      </p>
+
+      <h2>&hellip;and Sound</h2>
+
+      <p>
+        The picture is only half of the cinema experience, the other being the
+        sound. Warwick Student Cinema can play up to 5.1 surround; left, centre
+        and right channels provide precise and clear positioning of dialogue,
+        separate left and right surround channels help provide the feeling of
+        immersion in the sound and give general atmosphere and finally a
+        subwoofer/bass effects channel produces extra strong bass sounds which
+        are particularly audible in action and special effects sequences. All
+        our amplifiers are THX certified, as are our surround and subwoofer
+        speakers. Our left, right and centre loudspeaker stacks are not an
+        off-the-shelf system and were custom assembled from components by WSC
+        (carefully chosen to best work with the space in L3) and so have no
+        manufacturer THX certification as a whole, but only high-quality
+        components specified elsewhere in THX-certified systems are used.
+      </p>
+
+      <p>
+        Digital films all have a standard LPCM sound format, which is entirely
+        uncompressed and lossless in order to give the highest quality audio
+        possible. From film, we are capable of playing all industry-standard
+        sound formats; this includes all three major digital systems &ndash;
+        Dolby Digital, Digital Theatre Systems (DTS) and Sony Dynamic Digital
+        Sound (SDDS) &ndash; as well as the older analogue formats. Analogue
+        sound on 35mm film is usually recorded optically and comprises the
+        single-channel mono format as well as the stereo and analogue surround
+        sound Dolby A and Dolby SR formats. 70mm film is presented with either
+        fantastic six-channel Dolby magnetic sound or crystal-clear DTS digital
+        sound. Magnetic sound on 35mm is rare &ndash; but we can play it if the
+        need arises!
+      </p>
+
+      <p>
+        When showing either 35mm or 70mm film, the sound readers pick up the
+        information on the film print and send this to the relevant processor.
+        For most formats this is the CP500, which can decode mono, Dolby A,
+        Dolby SR and Dolby Digital sound &ndash; all of which share a combined
+        reader built into to the Victoria 8 projectors. The DTS and SDDS systems
+        both consist of separate readers (which are bolted onto the projectors
+        as required) and processors.
+      </p>
+
+      <p>
+        All sound is fed into our AP20 sound processor; when projecting film
+        this will come from the relevant film sound processor and for digital
+        shows this will come straight from the digital cinema server. The AP20
+        allows us to make adjustments to volume etc. and the Dirac Live 2.0
+        processing specially optimises the audio based on measurements of how
+        sound behaves in L3, ensuring that all of our shows sound as good as can
+        be. After processing, each channel of output goes into its own
+        amplifier, which together provide more than enough power to drive all of
+        the speakers around the auditorium.
+      </p>
+
+      <p>
+        But it is not just a crisp picture and clear sound that makes projection
+        at Warwick Student Cinema what it is today. All of this equipment would
+        be useless if it were not for the steady stream of dedicated technicians
+        who maintain the equipment and for the projectionists who run it. These
+        people all give up their time to first be trained and then, in turn,
+        take on the task of training subsequent generations.
+      </p>
+
+      <h2>Equipment</h2>
+      <ul>
+        <li>2x Cinemeccanica Victoria 8 35/70mm projectors</li>
+        <li>
+          1x Barco SP4K-15 RGB laser digital cinema projector with high-contrast
+          motorised zoom lens
+        </li>
+        <li>1x Barco Alchemy ICMP-X integrated cinema media processor</li>
+        <li>1x DTS XD10 film sound processor with AES digital output module</li>
+        <li>1x DTS 6DSV film sound processor as reserve</li>
+        <li>2x 35mm DTS readers and 2x 70mm DTS readers</li>
+        <li>
+          1x Dolby CP500-70 film sound processor and 2x CAT689 Dolby CP500
+          remotes
+        </li>
+        <li>1x Dolby MPU-1 mangnetic audio preamplifier</li>
+        <li>
+          2x Dolby/Cinemeccanica reverse scan analogue/digital 35mm sound
+          readers
+        </li>
+        <li>
+          1x Datasat AP20 sound processor with Dirac Live 2.0 room optimisation
+        </li>
+        <li>
+          1x Sony DFP-D3000 SDDS film sound processor and 2x Sony DFP-R3000 SDDS
+          penthouse 35mm sound readers
+        </li>
+        <li>
+          1x Sony DFP-D2000 SDDS processor and 2x DFP-R2000 penthouse 35mm SDDS
+          readers as reserve
+        </li>
+        <li>
+          5x QSC ISA450 and 3x QSC ISA750 professional amplifiers (giving just
+          over 11,000 watts total)
+        </li>
+        <li>
+          3x JBL loudspeaker stacks for left, centre and right channels, each
+          comprising 4638 LF and 2227/2352 HF sections
+        </li>
+        <li>4x JBL 4645B subwoofer loudspeakers</li>
+        <li>
+          1x JBL 5749 twin-driver subwoofer loudspeaker (effectively two 4645Bs
+          in one cabinet)
+        </li>
+        <li>8x JBL 8340a surround loudspeakers (4 per suround channel)</li>
+        <li>1x LANsat electronic content delivery server</li>
+        <li>1x UniqueX electronic content delivery server</li>
+        <li>4x Wharfedale Diamond 7.1 booth monitor loudspeakers</li>
+        <li>1x Marantz CD-48 CD player</li>
+        <li>1x Cinemeccanica electric rewinding bench</li>
+        <li>1x Philips ST-270 platter</li>
+        <li>1x Component Engineering MS100 booth monitor panel unit</li>
+      </ul>
+
+      <ul>
+        <li>
+          Dolby Digital and DTS equipment installed by
+          <a href="http://www.bell-theatre.com/">Bell Theatre Services</a>.
+        </li>
+        <li>
+          Additional audio equipment supplied by
+          <a href="http://www.soundassociates.co.uk/">Sound Associates</a>.
+        </li>
+        <li>
+          Lamps supplied by <a href="https://www.jackroe.com/">Jack Roe (CS)</a>
+          .
+        </li>
+        <li>
+          Electronics and other similar equipment from
+          <a href="https://uk.rs-online.com/web/">RS Components</a>.
+        </li>
+        <li>
+          Dolby and the Double-D symbol are trademarks of Dolby Laboratories
+          Licensing Corporation.
+        </li>
+        <li>
+          Platter kindly loaned by the{' '}
+          <a href="http://www.ppttrust.org/">Projected Picture Trust</a>.
+        </li>
+        <li>
+          DFP-2000 Sony Dynamic Digital Sound (SDDS) equipment kindly supplied
+          on permanent loan by
+          <a href="https://web.archive.org/web/20130701055758/http://www.sdds.com/">
+            Sony UK
+          </a>
+          .
+        </li>
+        <li>Digital projector supplied and installed by Omnex Pro-Film.</li>
+      </ul>
+
+      <p>
+        <i>
+          Originally by Amanda Window (technical team); updated by Timothy Green
+          (chief projectionist 2011&ndash;12) and subsequently by Kieran Hall
+          (chief projectionist 2017&ndash;19).
+        </i>
+      </p>
     </main>
   );
 }
