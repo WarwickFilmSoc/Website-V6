e23b1cdf93ae53eb87a947ce3fc9b653e8bfa89b
Josh Heng <josh@joshheng.co.uk>
2023-08-27 20:19:27 +0100

    :wrench: Add additional website redirects

--- next.config.js
+++ next.config.js
@@ -16,6 +16,58 @@ const nextConfig = {
       destination: '/terms-and-conditions',
       permanent: true,
     },
+    {
+      source: '/membership',
+      destination: '/about#membership',
+      permanent: true,
+    },
+    {
+      source: '/advertise',
+      destination: '/about#advertise',
+      permanent: true,
+    },
+    {
+      source: '/find',
+      destination: '/about#find',
+      permanent: true,
+    },
+    {
+      source: '/contact',
+      destination: '/about',
+      permanent: true,
+    },
+
+    // Website V2 About
+    {
+      source: '/info',
+      destination: '/about',
+      permanent: true,
+    },
+    {
+      source: '/info/tickets',
+      destination: '/about#tickets',
+      permanent: true,
+    },
+    {
+      source: '/info/advertise',
+      destination: '/about#advertise',
+      permanent: true,
+    },
+    {
+      source: '/info/executive',
+      destination: '/executive',
+      permanent: true,
+    },
+    {
+      source: '/info/faqs',
+      destination: '/faq',
+      permanent: true,
+    },
+    {
+      source: '/info/contactus',
+      destination: '/about',
+      permanent: true,
+    },
 
     // Website V2 Constitution
     {
