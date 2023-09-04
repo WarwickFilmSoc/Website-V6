da20f9785d175711bb9aeef60d17b71789ef0ea8
Josh Heng <josh@joshheng.co.uk>
2023-09-04 13:12:59 +0100

    :sparkles: Add publicity page

--- src/app/(whats-on)/publicity/page.tsx
+++ src/app/(whats-on)/publicity/page.tsx
@@ -1,19 +1,67 @@
 import Link from 'next/link';
 import { Metadata } from 'next';
+import { execTeam, getExecPositionNames } from '@/data/exec';
+import publicityImage from '@/assets/publicity/publicity.jpg';
+import TeamOfficerCard from '@/app/crew/team-officer-card';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'Publicity',
-  description: '',
+  description:
+    "Publicity for the current term can be collected for free at our publicity table in the Science Concourse. However, if you'd also like to download a digital version or see our past publicity, you can find it here!",
 };
 
 export default function Publicity() {
   return (
     <main>
-      <p className="text-xl font-lexend uppercase -mb-1">
-        <Link href="/whats-on">What&apos;s On</Link>
+      <h1>Publicity</h1>
+      <Image
+        src={publicityImage}
+        alt="The publicity table in front of L3"
+        width={320}
+        className="lg:float-right relative lg:ml-4 mb-4"
+        placeholder="blur"
+        priority
+      />
+      <p className="mb-2">
+        Every term, the&nbsp;
+        <Link
+          href="https://dev.warwick.film/crew/publicity"
+          className="text-accent"
+        >
+          publicity team
+        </Link>
+        &nbsp;produce a range of materials to advertise our events and ensure
+        that everyone around campus knows about them. These are then sent to
+        print and distributed at both our publicity table outside L3 and in
+        various pub runs and other events throughout the year.
       </p>
-      <h1 className="mb-1">Publicity</h1>
-      <p className="mb-2">Page Content</p>
+      <p>
+        From writing reviews to editing graphics to distributing publicity,
+        there&apos;s always a huge range of jobs that need doing - if you&apos;d
+        like to get involved,&nbsp;
+        <a
+          href={`mailto:${execTeam.publicityOfficer.email}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-accent"
+        >
+          email the {execTeam.publicityOfficer.name},&nbsp;
+          {getExecPositionNames(execTeam.publicityOfficer)}
+        </a>{' '}
+        for more information.
+      </p>
+      <div className="mt-2 flex">
+        <TeamOfficerCard position={execTeam.publicityOfficer} />
+      </div>
+
+      <h2 className="mt-8">Publicity Archive</h2>
+      <p>
+        Publicity for the current term can be collected for free at our
+        publicity table in the Science Concourse. However, if you&apos;d also
+        like to download a digital version or see our past publicity, you can
+        find it here!
+      </p>
     </main>
   );
 }
