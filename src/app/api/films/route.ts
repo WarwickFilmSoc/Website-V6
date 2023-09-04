dbedf1922516defc499cc4c6681898dbb3e88bcd
Arun Pattni <arun.pattni@internal.filmsoc.warwick.ac.uk>
2023-09-04 11:14:48 +0000

    💄 Style film search

--- src/app/api/films/route.ts
+++ src/app/api/films/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
-import { FilmSearchResponse } from '@/app/api/films/types';
+import { FilmSearchResponse, FilmSearchResult } from '@/app/api/films/types';
 
 export async function GET(request: NextRequest) {
   const search = request.nextUrl.searchParams.get('search');
@@ -23,6 +23,8 @@ export async function GET(request: NextRequest) {
       title: true,
       year: true,
       film_id: true,
+      tmdb_backdrop_path: true,
+      tmdb_genres: true,
       screenings: {
         orderBy: {
           timestamp: 'desc',
@@ -37,8 +39,24 @@ export async function GET(request: NextRequest) {
     take: 100,
   });
 
+  let past: FilmSearchResult[] = [];
+  let future: FilmSearchResult[] = [];
+  const currentDate = Date.now();
+  for (const film of films) {
+    if (film.screenings.length > 0 && film.screenings[0].timestamp) {
+      const screeningDate = Number(film.screenings[0].timestamp) * 1000;
+
+      screeningDate > currentDate ? future.push(film) : past.push(film);
+    } else {
+      past.push(film);
+    }
+  }
+
   return NextResponse.json<FilmSearchResponse>({
     success: true,
-    data: films,
+    data: {
+      past: past,
+      future: future,
+    },
   });
 }
---
+++ src/components/films/ScreeningsHr.tsx
@@ -0,0 +1,9 @@
+export default function ScreeningsHr({ text }: { text: string }) {
+  return (
+    <div className="flex items-center md:mx-4 mb-6">
+      <hr className="grow border-t-2 m-2 md:m-4 flex-shrink-0 w-4" />
+      <h2 className="text-xl md:text-2xl mx-2 md:mx-4 text-center">{text}</h2>
+      <hr className="grow border-t-2 m-2 md:m-4 flex-shrink-0 w-4" />
+    </div>
+  );
+}
