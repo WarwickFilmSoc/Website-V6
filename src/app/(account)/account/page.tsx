a9bbf8a5a61dde7374a9ab29f148cb41195af63c
Josh Heng <josh@joshheng.co.uk>
2023-09-11 15:54:45 +0100

    :sparkles: Add account details page and add password change page template

--- src/app/(account)/account/page.tsx
+++ src/app/(account)/account/page.tsx
@@ -9,7 +9,7 @@ import { Metadata } from 'next';
 export const revalidate = 0; // Revalidate every 10m
 export const metadata: Metadata = {
   title: 'Your Account',
-  description: 'View your account and past screening history',
+  description: 'View your account and past ticket history',
 };
 
 export default async function Account() {
@@ -20,36 +20,106 @@ export default async function Account() {
 
   return (
     <main>
-      <h1 className="mb-1">Your Account</h1>
-      <p className="mb-2">Welcome, {user.person.forename}!</p>
+      <div className="flex gap-8 lg:gap-4 flex-col lg:flex-row">
+        <div>
+          <h1>Your Account</h1>
 
-      {isCrew ? (
-        <p>
-          Looking for crew resources or the films you&apos;ve worked? Find them
-          on&nbsp;
+          <p className="mb-2">Welcome, {user.person.forename}!</p>
+
+          {isCrew ? (
+            <p>
+              Looking for crew resources or the films you&apos;ve worked? Find
+              them on&nbsp;
+              <a
+                href="https://crew.warwick.film"
+                className="text-accent"
+                target="_blank"
+                rel="noopener"
+              >
+                crew.warwick.film
+              </a>
+              .
+            </p>
+          ) : (
+            <p>
+              You are not currently crew. Find out more about crew{' '}
+              <Link href="/crew" className="text-accent">
+                here
+              </Link>
+              .
+            </p>
+          )}
+
+          <LargeButtonLink href="/logout" className="mt-4">
+            Logout
+          </LargeButtonLink>
+        </div>
+
+        <div className="lg:ml-auto lg:text-right flex-shrink-0">
+          <h2>Account Details</h2>
+          <table className="mt-1 lg:ml-auto table-style">
+            <tbody>
+              <tr>
+                <th>Name (From SU)</th>
+                <td className="text-left">
+                  {user.person.forename} {user.person.surname}
+                </td>
+              </tr>
+              <tr>
+                <th>Email (From SU)</th>
+                <td className="text-left">{user.person.email}</td>
+              </tr>
+              <tr>
+                <th>Web Username</th>
+                <td className="text-left">{user.login}</td>
+              </tr>
+              <tr>
+                <th>Web Password</th>
+                <td className="text-left">
+                  <Link href="/account/change-password" className="text-accent">
+                    Change
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <h2 className="mt-8">Mailing Preferences</h2>
+      <p>
+        The e-mail address given here will be used to send out Warwick Student
+        Cinema mailings with details of showings and special events. We do not
+        share your email address with third parties.
+      </p>
+      {user.person.autoupdate ? (
+        <div className="mt-2">
+          Your mailing preferences are automatically provided by the Student
+          Union. To change your email address, please do so on the&nbsp;
           <a
-            href="https://crew.warwick.film"
-            className="text-accent"
+            href="https://www.warwicksu.com/contactdetails/"
             target="_blank"
             rel="noopener"
+            className="text-accent"
           >
-            crew.warwick.film
+            Student Union&apos;s website
           </a>
           .
-        </p>
+        </div>
       ) : (
-        <p>
-          You are not currently crew. Find out more about crew{' '}
-          <Link href="/crew" className="text-accent">
+        <div className="mt-2">
+          Please update your mailing preferences&nbsp;
+          <a
+            href="https://warwick.film/contacts/edit"
+            target="_blank"
+            rel="noopener"
+            className="text-accent"
+          >
             here
-          </Link>
+          </a>
           .
-        </p>
+        </div>
       )}
-
-      <LargeButtonLink href="/logout" className="mt-4">
-        Logout
-      </LargeButtonLink>
 
       <h2 className="mt-8">Shows Watched</h2>
       <Suspense fallback={<FilmLayout loading />}>
